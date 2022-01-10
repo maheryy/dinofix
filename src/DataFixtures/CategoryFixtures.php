d1d0978b59032dea0b5bb1bcc05b9d09dbdf047a
calintha <cal.intha77@gmail.com>
2022-01-10 21:41:22 +0100

    feat: Create category dino and user fixtures

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = \Faker\Factory::create('fr_FR');
+        
+        $object = (new Category())
+            ->setName('Aquatique')
+            ->setDescription($faker->sentence(20))
+            ->setSlug('aquatique')
+            ->setPicture('no pic');
+        $manager->persist($object);
+
+        $object = (new Category())
+            ->setName('Terrestre')
+            ->setDescription($faker->sentence(20))
+            ->setSlug('terrestre')
+            ->setPicture('no pic');
+        $manager->persist($object);
+
+        $object = (new Category())
+            ->setName('Volant')
+            ->setDescription($faker->sentence(20))
+            ->setSlug('volant')
+            ->setPicture('no pic');
+        $manager->persist($object);
+
+        $manager->flush();
+    }
+}
---
+++ src/DataFixtures/DinoFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use App\Entity\Dino;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class DinoFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = \Faker\Factory::create('fr_FR');
+
+        $categories = $manager->getRepository(Category::class)->findAll();
+
+        for($i = 0; $i < 25; $i++) {
+            $object = (new Dino())
+                ->setName($faker->word)
+                ->setDescription($faker->sentence(20))
+                ->setSlug($faker->word)
+                ->setPicture('no pic');
+            for($j = 0; $j < $faker->numberBetween(1,3); $j++) {
+                $object->setCategory($faker->randomElement($categories));
+            }
+            $manager->persist($object);
+        }
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            CategoryFixtures::class
+        ];
+    }
+}
