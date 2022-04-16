1d6314bd4a22191a29d4fdf9957ff1f6c641559e
Prado <mahery.rsh@gmail.com>
2022-04-16 12:06:05 +0200

    imp: users can now switch from customer to fixer easily

--- src/Service/ResolverService.php
+++ src/Service/ResolverService.php
@@ -16,7 +16,7 @@ class ResolverService
     }
 
 
-    public function getFixerExpertise($fixerId) : array
+    public function getFixerExpertise($fixerId): array
     {
         $res = [];
         $services = $this->serviceRepository->findAllFixerServices($fixerId);
@@ -25,10 +25,12 @@ class ResolverService
         }
 
         foreach ($services as $service) {
-            $category = $service->getCategory();
-            $dino = $service->getDino();
-            $res['categories'][$category->getId()] = $category;
-            $res['dinos'][$dino->getId()] = $dino;
+            if ($category = $service->getCategory()) {
+                $res['categories'][$category->getId()] = $category;
+            }
+            if ($dino = $service->getDino()) {
+                $res['dinos'][$dino->getId()] = $dino;
+            }
         }
 
         $res['categories'] = array_unique($res['categories']);
