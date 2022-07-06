ab0ec218f7844734d2398e1f9fb2d9378faff451
calintha <cal.intha77@gmail.com>
2022-07-06 12:38:56 +0200

    feat: Add profile security

--- src/Controller/Customer/CustomerSecurityController.php
+++ src/Controller/Customer/CustomerSecurityController.php
@@ -14,9 +14,9 @@ class CustomerSecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-         if ($this->getUser()) {
-             return $this->redirectToRoute('customer_home');
-         }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('customer_home');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
