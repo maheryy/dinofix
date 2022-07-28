c0d55f3fd135eb917dabdf3bd5538d3040525bde
Sadik Raida <sadik.raida@gmail.com>
2022-07-28 02:43:48 +0200

    feat : edit password Customer with hash

--- src/Controller/Customer/ProfileController.php
+++ src/Controller/Customer/ProfileController.php
@@ -11,17 +11,26 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class ProfileController extends AbstractController
 {
     #[Route('/account/edit', name: 'profile_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, UserPasswordHasherInterface $customerPasswordHasher, EntityManagerInterface $entityManager): Response
     {
         $customer = $this->getUser();
         $form = $this->createForm(CustomerType::class, $customer);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            
+            $customer->setPassword(
+                $customerPasswordHasher->hashPassword(
+                    $customer,
+                    $form->get('password')->getData()
+                )
+            );
             $entityManager->flush();
 
             $this->addFlash('success', 'Votre profil a été modifié avec succés !');
